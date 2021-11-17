5d2d1ee292de279476b076b484417e3e486c64e4
yigexiaobing <1459014701@qq.com>
2021-11-17 13:09:35 +0800

    tj

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -2,19 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const handler = (percentage, message, ...args) => {
-  // e.g. Output each progress message directly to the console:
-  console.info(percentage * 100 + '%');
-};
 
 module.exports = {
+  context: path.resolve(__dirname),
   entry: {
     app: [path.resolve(__dirname, '../src/main.js')],
   },
-  context: path.resolve(__dirname),
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
@@ -33,6 +26,5 @@ module.exports = {
       template: path.resolve(__dirname, '../public/index.html'),
       favicon: path.resolve(__dirname, '../public/favicon.ico'),
     }),
-    // new FriendlyErrorsWebpackPlugin(),
   ],
 };
