162e55ba0e05fb65b75a36017229fdf671b425d9
yigexiaobing <1459014701@qq.com>
2021-11-21 21:22:27 +0800

    no message

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -21,17 +21,17 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/i,
-      //   exclude: /(node_modules)/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: ['@babel/preset-env'],
-      //       plugins: ['@babel/plugin-transform-runtime'],
-      //     },
-      //   },
-      // },
+      {
+        test: /\.js$/i,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
     ],
   },
   plugins: [
