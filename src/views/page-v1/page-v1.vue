5781823d80bf2c04a418c22cfb7cee1c073fd286
yigexiaobing <1459014701@qq.com>
2021-11-21 21:34:14 +0800

    no message

--- src/views/page-v1/page-v1.vue
+++ src/views/page-v1/page-v1.vue
@@ -3,27 +3,33 @@
 </template>
 
 <script>
-  import cloneDeep from 'lodash/cloneDeep';
+import cloneDeep from 'lodash/cloneDeep';
 
-  export default {
-    name: 'page-v1',
-    data() {
-      return {
-        obj: {
-          id: 1,
-          name: 'page-v1',
-        },
-      };
-    },
-    created() {
-      const copy = cloneDeep(this.obj);
-      console.log(copy);
-    },
-  };
+export default {
+  name: 'page-v1',
+  data() {
+    return {
+      obj: {
+        id: 1,
+        name: 'page-v1',
+      },
+      list: ['1', 2, { id: 1 }],
+    };
+  },
+  created() {
+    const copy = cloneDeep(this.obj);
+    const { name } = this.obj;
+    const list = [...this.list];
+    const fn = () => {
+      console.log('fn unused');
+    };
+    console.log(copy, name, list);
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .page-v1 {
-    padding: 10px;
-  }
+.page-v1 {
+  padding: 10px;
+}
 </style>
