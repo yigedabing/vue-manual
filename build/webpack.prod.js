4e1ae1d411de69694be7dd5e022b75a8ee6c5796
yigexiaobing <1459014701@qq.com>
2021-11-21 12:44:08 +0800

    4

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
