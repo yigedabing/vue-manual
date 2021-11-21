aeb3e260421cba40e31a0d0c87e8cff1710448bf
yigexiaobing <1459014701@qq.com>
2021-11-21 22:10:15 +0800

    no message

--- src/views/page-v2/page-v2.vue
+++ src/views/page-v2/page-v2.vue
@@ -53,70 +53,75 @@
 </template>
 
 <script>
-  import helloWorld from '@/components/hello-wrold.vue';
+import helloWorld from '@/components/hello-wrold.vue';
 
-  export default {
-    name: 'page-v2',
-    components: { helloWorld },
-    data() {
-      return {
-        title: '欢迎王者归来！',
-        msg: Math.random(),
-      };
+export default {
+  name: 'page-v2',
+  components: { helloWorld },
+  data() {
+    return {
+      title: '欢迎王者归来！',
+      msg: Math.random(),
+      list: [1, 2, { id: 1 }, [{ id: 12 }, { id: 13 }]],
+    };
+  },
+  created() {
+    const list = [...this.list];
+    console.log(list === this.list);
+  },
+  methods: {
+    update() {
+      this.msg = Math.random();
     },
-    methods: {
-      update() {
-        this.msg = Math.random();
-      },
-      goNewPage() {
-        this.$router.push('/page-v3');
-      },
+    goNewPage() {
+      this.$router.push('/page-v3');
     },
-  };
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .page-v2 {
-    .image-list {
-      background-color: #dedede;
-      display: flex;
-      flex-flow: row wrap;
-      img {
-        width: 100px;
-        margin: 10px;
-        padding: 4px;
-        border: 1px solid red;
-      }
-    }
-    .bgc-image {
-      display: flex;
-      justify-content: space-between;
-      li {
-        width: 100px;
-        height: 100px;
-      }
-      .bgc-1 {
-        width: 100px;
-        height: 100px;
-        background: url(@/assets/images/1.jpeg) center/contain no-repeat;
-      }
-      .bgc-2 {
-        background: url(@/assets/images/6.gif) center/contain no-repeat;
-      }
-      .bgc-3 {
-        background: url(@/assets/images/8.png) center/contain no-repeat;
-      }
-      .bgc-4 {
-        background: url(@/assets/images/21.jpeg) center/contain no-repeat;
-      }
-    }
-    .font {
-      i {
-        color: red;
-        font-size: 20px;
-        font-weight: 700;
-        margin: 20px;
-      }
+.page-v2 {
+  .image-list {
+    background-color: #dedede;
+    display: flex;
+    flex-flow: row wrap;
+    img {
+      width: 100px;
+      margin: 10px;
+      padding: 4px;
+      border: 1px solid red;
     }
   }
+  .bgc-image {
+    display: flex;
+    justify-content: space-between;
+    li {
+      width: 100px;
+      height: 100px;
+    }
+    .bgc-1 {
+      width: 100px;
+      height: 100px;
+      background: url(@/assets/images/1.jpeg) center/contain no-repeat;
+    }
+    .bgc-2 {
+      background: url(@/assets/images/6.gif) center/contain no-repeat;
+    }
+    .bgc-3 {
+      background: url(@/assets/images/8.png) center/contain no-repeat;
+    }
+    .bgc-4 {
+      background: url(@/assets/images/21.jpeg) center/contain no-repeat;
+    }
+  }
+  .font {
+    i {
+      color: red;
+      font-size: 20px;
+      font-weight: 700;
+      margin: 20px;
+    }
+  }
+}
 </style>
