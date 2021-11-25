b2ff0c9074590ea4027a7bc67849d4fc83398f12
yigexiaobing <1459014701@qq.com>
2021-11-25 13:15:15 +0800

    使用 postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
-    // ['postcss-preset-env', {}],
-    ['autoprefixer', {}],
+    // 添加css样式前缀，下面2个插件二选一
+    // postcss-preset-env includes autoprefixer,so adding it separately is not necessary if you already use the preset.
+    ['postcss-preset-env', {}],
+    // ['autoprefixer', {}],
   ],
 };
