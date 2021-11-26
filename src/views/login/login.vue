9926cbeb676dd6faa3ac5da0a47e0c398f572dbf
yigexiaobing <1459014701@qq.com>
2021-11-26 20:21:09 +0800

    no message

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -5,13 +5,10 @@
       <i class="el-icon-video-play"></i>
     </el-button>
     <pre-loader :isLoading="isLoading"></pre-loader>
-    <div id="circle-echarts"></div>
   </div>
 </template>
 
 <script>
-import * as echarts from 'echarts';
-
 export default {
   name: 'login',
   data() {
@@ -19,49 +16,7 @@ export default {
       isLoading: false,
     };
   },
-  mounted() {
-    const chartDom = document.getElementById('circle-echarts');
-    const myChart = echarts.init(chartDom);
-    let option;
-
-    option = {
-      title: {
-        text: 'Referer of a Website',
-        subtext: 'Fake Data',
-        left: 'center',
-      },
-      tooltip: {
-        trigger: 'item',
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left',
-      },
-      series: [
-        {
-          name: 'Access From',
-          type: 'pie',
-          radius: '50%',
-          data: [
-            { value: 1048, name: 'Search Engine' },
-            { value: 735, name: 'Direct' },
-            { value: 580, name: 'Email' },
-            { value: 484, name: 'Union Ads' },
-            { value: 300, name: 'Video Ads' },
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)',
-            },
-          },
-        },
-      ],
-    };
-
-    option && myChart.setOption(option);
-  },
+  mounted() {},
   methods: {
     login() {
       this.isLoading = true;
@@ -81,12 +36,6 @@ export default {
   font-size: 16px;
   .icon {
     margin: 20px 0;
-  }
-  #circle-echarts {
-    width: 800px;
-    height: 600px;
-    margin: 50px auto 0;
-    border: 1px solid red;
   }
 }
 </style>
--- src/views/page-v1/page-v1.vue
+++ src/views/page-v1/page-v1.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="page-v1">page-v1</div>
+  <div class="page-v1">
+    <div>page-v1</div>
+    <div id="circle-echarts"></div>
+  </div>
 </template>
 
 <script>
 import cloneDeep from 'lodash/cloneDeep';
+import * as echarts from 'echarts';
 
 export default {
   name: 'page-v1',
@@ -25,11 +29,60 @@ export default {
     };
     console.log(copy, name, list);
   },
+  mounted() {
+    const chartDom = document.getElementById('circle-echarts');
+    const myChart = echarts.init(chartDom);
+    let option;
+
+    option = {
+      title: {
+        text: 'Referer of a Website',
+        subtext: 'Fake Data',
+        left: 'center',
+      },
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left',
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            },
+          },
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  },
 };
 </script>
 
 <style lang="less" scoped>
 .page-v1 {
   padding: 10px;
+  #circle-echarts {
+    width: 800px;
+    height: 600px;
+    margin: 50px auto 0;
+    border: 1px solid red;
+  }
 }
 </style>
