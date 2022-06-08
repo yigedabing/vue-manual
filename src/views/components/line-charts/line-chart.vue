0fee2bd3249a276c81d0fc96ce699a4893e86a55
yigexiaobing <1459014701@qq.com>
2022-06-08 15:33:47 +0800

    test

---
+++ src/views/components/line-charts/line-chart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="line-chart">
+    <div>测试按用户</div>
+
+    <head>
+      啊的防晒霜
+    </head>
+    <span>
+      {{ id }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'line-chart',
+  data() {
+    return {
+      id: 'abc',
+    };
+  },
+  created(){
+    console.log(`created`)
+  }
+};
+</script>
+
+<style></style>
